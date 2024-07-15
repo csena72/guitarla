c154d558900339f4137cd8715ae9ab4a97039649
Cristian Sena <crisianjsena@gmail.com>
2024-07-15 19:30:03 -0300

    Agrego productos desde db

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { Guitar } from "./components/Guitar";
+import { db } from "./data/db";
+
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    setData(db)
+  }, [])
 
   return (
     <>
@@ -12,7 +21,11 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar />
+          {
+            data.map((guitar) => (
+              <Guitar key={guitar.id} {...guitar} />
+            ))
+          }
         </div>
         
       </main>
